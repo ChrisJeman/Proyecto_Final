6aab0077fe84c72d53943082cb76bca5dc5e5c70
Saavedra0206 <itta.saavedra@utec.edu.pe>
2026-05-07 00:56:18 -0500

    Carga de la parte Integrante 1

---
+++ LecturaCSV.h
@@ -0,0 +1,19 @@
+//
+// Created by ITALO on 6/05/2026.
+//
+
+#ifndef PROYECTOPART1_LECTURACSV_H
+#define PROYECTOPART1_LECTURACSV_H
+
+
+#include <vector>
+#include <string>
+
+#include "PELIS.h"
+
+using namespace std;
+
+vector<Pelis> cargarPelis(string filename);
+
+
+#endif //PROYECTOPART1_LECTURACSV_H
